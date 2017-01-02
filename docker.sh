464cd54eb086b54c7750f79942645cc7c2a287fd
thorpelawrence <thorpe_lawrence@yahoo.co.uk>
2017-01-02 21:38:01 +0000

    Update docker script to add volume mounting

--- docker.sh
+++ docker.sh
@@ -3,4 +3,4 @@ docker stop glitchbot
 docker rm glitchbot
 docker rmi glitchbot
 docker build -t glitchbot .
-docker run -d --name glitchbot glitchbot
+docker run -d --name glitchbot -v GlitchBot/bin/Debug/music:/glitchbot/bin/Debug/music glitchbot
